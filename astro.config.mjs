598f27f463e40a3f616e95c23da5b1854de442a7
“dimasjalintraksi” <dimasmaspur@gmail.com>
2025-12-31 17:15:22 +0700

    seo adjustment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,27 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kandela.tech',
-  
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [
-    react(),
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      customPages: [
-        'https://kandela.tech/',
-        'https://kandela.tech/projects',
-        'https://kandela.tech/career'
-      ]
-    })
-  ]
+  integrations: [react()]
 });
